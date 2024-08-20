6dbc6d836651da4d0e057e2cec66d0847ce1dec4
Djair Silva <djavasilva@yahoo.com.br>
2024-08-20 12:16:51 -0300

    todo app

--- user_data.sh
+++ user_data.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # Criar usuário template com senha Te#mpl@te e dar permissões de root
-useradd template #criando usuario
-echo 'template:Te#mpl@te' | chpasswd #senhad o usuario
-echo 'root:Dd33557788' | chpasswd #senhad o usuario
-usermod -aG wheel template #permissão de root para o usuario
+useradd template # criando usuario
+echo 'template:Te#mpl@te' | chpasswd # senha do usuario
+echo 'root:Dd33557788' | chpasswd # senha do usuario
+usermod -aG wheel template # permissão de root para o usuario
 
 # Habilitar login com usuário e senha
-sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config  #habilitando acesso com senha via ssh
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config #habilitando acesso com senha via ssh
-sed -i 's/^#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config #habilitando acesso com senha via ssh
-sed -i 's/^PermitEmptyPasswords yes/PermitEmptyPasswords no/' /etc/ssh/sshd_config #habilitando acesso com senha via ssh
+sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config  # habilitando acesso com senha via ssh
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config # habilitando acesso com senha via ssh
+sed -i 's/^#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config # habilitando acesso com senha via ssh
+sed -i 's/^PermitEmptyPasswords yes/PermitEmptyPasswords no/' /etc/ssh/sshd_config # habilitando acesso com senha via ssh
 
 # Reiniciar o serviço SSH para aplicar as mudanças
 systemctl restart sshd
 
 # Instalar Apache e unzip
-sudo yum update -y
-sudo yum install httpd unzip -y
+yum update -y
+yum install httpd unzip -y
 
-#Instalar Docker e Git
-sudo yum update -y
-sudo yum install git -y
-sudo yum install docker -y
-sudo usermod -a -G docker template
+# Instalar Docker e Git
+yum update -y
+yum install git -y
+yum install docker -y
+usermod -a -G docker template
 id template
-sudo newgrp docker
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo /home/ec2-user/bia/build.sh
+newgrp docker
+systemctl start httpd
+systemctl enable httpd
+/home/ec2-user/bia/build.sh
 
 # Criar arquivo de configuração do Apache para o site
-cat <<EOL | sudo tee /etc/httpd/conf.d/teste1.conf
+cat <<EOL | tee /etc/httpd/conf.d/teste1.conf
 <VirtualHost *:80>
     DocumentRoot "/var/www/html/teste1"
     ServerName localhost
@@ -43,33 +43,19 @@ cat <<EOL | sudo tee /etc/httpd/conf.d/teste1.conf
 </VirtualHost>
 EOL
 
-# Reiniciar o serviço Apache para aplicar as mudanças
-sudo systemctl restart httpd
-
-# Ativar docker
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+# Parar o serviço Apache para liberar a porta 80
+systemctl stop httpd
 
 # Instalar docker compose 2
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
 
 # Validar instalação do Docker Compose
 docker compose version
 
 # Instalar node e npm
-curl -fsSL https://rpm.nodesource.com/setup_21.x | sudo bash -
-sudo yum install -y nodejs
+curl -fsSL https://rpm.nodesource.com/setup_21.x | bash -
+yum install -y nodejs
 
-# Clonar o repositório do GitHub e copiar o conteúdo da pasta 'site' para '/mnt'
-git clone https://github.com/seu-usuario/seu-repositorio.git /tmp/sit
-cp -r /tmp/seu-repositorio/site /mnt
-
-# Remover a pasta temporária
-rm -rf /tmp/seu-repositorio
-
-# Entrar na pasta copiada e iniciar o server.js com o Node.js
-cd /mnt/site
-npm install  # Instalar dependências do Node.js
-node server.js
